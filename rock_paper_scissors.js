93bdefce528a84cdced738fab9d55137d59b21c2
CamDiggidyDawg <camrcrichton@gmail.com>
2023-11-13 15:45:35 +0000

    Update js file with result function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -4,11 +4,13 @@ const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice;
 let computerChoice;
+let result;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice()
+    getResult()
 }));
 
 function generateComputerChoice() {
@@ -23,4 +25,24 @@ function generateComputerChoice() {
     };
 
     computerChoiceDisplay.innerHTML = computerChoice;
+};
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = "It's a draw!";
+    } else if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = "You win!";
+    } else if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = "You lost!";
+    } else if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = "You lost!";
+    } else if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = "You win!";
+    } else if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = "You win!";
+    } else if ( computerChoice === 'scissors' && userChoice === 'paper') {
+        result = "You lose!";
+    };
+
+    resultDisplay.innerHTML = result;
 };
